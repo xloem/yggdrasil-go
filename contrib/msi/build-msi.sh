6878366311c3654afba3f70fc09c377ff8bd9d61
Neil Alexander <neilalexander@users.noreply.github.com>
2022-04-17 21:54:40 +0100

    Build some packages

--- contrib/msi/build-msi.sh
+++ contrib/msi/build-msi.sh
@@ -51,7 +51,7 @@ fi
 [ "${PKGARCH}" == "x64" ] && GOOS=windows GOARCH=amd64 CGO_ENABLED=0 ./build
 [ "${PKGARCH}" == "x86" ] && GOOS=windows GOARCH=386 CGO_ENABLED=0 ./build
 [ "${PKGARCH}" == "arm" ] && GOOS=windows GOARCH=arm CGO_ENABLED=0 ./build
-#[ "${PKGARCH}" == "arm64" ] && GOOS=windows GOARCH=arm64 CGO_ENABLED=0 ./build
+[ "${PKGARCH}" == "arm64" ] && GOOS=windows GOARCH=arm64 CGO_ENABLED=0 ./build
 
 # Create the postinstall script
 cat > updateconfig.bat << EOF
